171b860669b807a3d6450dbcffbd146ada96711b
mubacher <mubacheralrifai@gmail.com>
2020-06-07 19:01:07 +0200

    Devuelve jsons

--- run.py
+++ run.py
@@ -1,7 +1,33 @@
 from flask import Flask
+import csv
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hola, mundo"
+    fVentas = open('./sales10.csv', 'r')
+    csvreader = csv.reader(fVentas, delimiter=',')
+
+    registros = []
+    d = {}   
+    for linea in csvreader:
+        registros.append(linea)
+        if linea[0] in d:
+            d[linea[0]]['ingresos'] += float(linea[11])
+            d[linea[0]]['beneficios'] += float(linea[13])
+        else:
+            if linea[0] != 'region':
+                d[linea[0]] = {'ingresos':float(linea[11]), 'beneficios':float(linea[13])}
+
+    return d
+
+    '''
+    <abrir fichero>
+    <mientras haya registros>
+        registro = leerFichero
+        registro. separar.por comas
+        procesar ventas totales y beneficios totales
+
+    Montar respuesta
+    devolver respuesta.
+    '''
